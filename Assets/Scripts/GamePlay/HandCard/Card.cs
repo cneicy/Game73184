c89d5e3657054c8170909b9805f705b475a01dce
Faithbian888 <161805389+Faithbian888@users.noreply.github.com>
2025-08-01 20:03:17 +0800

    建造1

--- Assets/Scripts/GamePlay/HandCard/Card.cs
+++ Assets/Scripts/GamePlay/HandCard/Card.cs
@@ -3,14 +3,16 @@ using UnityEngine.EventSystems;
 
 namespace GamePlay.HandCard
 {
-    public class Card : MonoBehaviour,IPointerEnterHandler,IPointerExitHandler
+    public class Card : MonoBehaviour,IPointerEnterHandler,IPointerExitHandler,IInteractable
     {
         [SerializeField]private Vector3 _cardLocalPosition;
         private Transform _localTransform;
         private SpriteRenderer _spriteRenderer;
+        private bool _isSelected;
 
         private void Start()
         {
+            _isSelected = false;
             _cardLocalPosition = transform.localPosition;
             _localTransform = transform;
             _spriteRenderer = GetComponent<SpriteRenderer>();
@@ -24,6 +26,22 @@ namespace GamePlay.HandCard
         public void OnPointerExit(PointerEventData eventData)
         {
             CardHover.Instance.MouseExit(_cardLocalPosition,_localTransform,_spriteRenderer);
+        }
+
+        public void Interact()
+        {
+            if (!_isSelected)
+            {
+                _cardLocalPosition = transform.localPosition;
+                CardHover.Instance.MouseEnter(_cardLocalPosition,_localTransform,_spriteRenderer);
+                _isSelected = true;
+            }
+            else
+            {
+                _cardLocalPosition = transform.localPosition;
+                CardHover.Instance.MouseExit(_cardLocalPosition,_localTransform,_spriteRenderer);
+                _isSelected = false;
+            }
         }
     }
 }
